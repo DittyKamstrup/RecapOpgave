0007538c4700a7c760ab9f68a36a1a3eca0b13f3
Ditte <Ditte@DESKTOP-QORFADN>
2017-08-27 22:54:48 +0200

    Unit test project added and test method "AgePartinioningTest()" added

---
+++ RecapOpgaveUnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace RecapOpgaveUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AgePartinioningTest()
+        {
+            //Arrange
+            RecapOpgave.Member member = new RecapOpgave.Member("Søs", 14);
+            //Act
+            member.AgePartitioning();
+            //Assert
+            Assert.AreEqual(member.Team,"Pandaholdet");
+        }
+    }
+}
