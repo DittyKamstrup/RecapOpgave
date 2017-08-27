98126c71c1c143145b89552e996c00bb2d31ffad
Ditte <Ditte@DESKTOP-QORFADN>
2017-08-27 22:41:22 +0200

    "Program" class "done" - trying out the method in a foreach loop

--- RecapOpgave/Program.cs
+++ RecapOpgave/Program.cs
@@ -8,8 +8,24 @@ namespace RecapOpgave
 {
     class Program
     {
+
+        public static List<Member> MemberList { get; set; }
         static void Main(string[] args)
         {
+            MemberList = new List<Member>();
+
+            Member member1 = new Member("Pyrus", 147);
+            Member member2 = new Member("Nicklas", 17);
+            Member member3 = new Member("Ditte", 24);
+
+            MemberList.Add(member1);
+            MemberList.Add(member2);
+            MemberList.Add(member3);
+
+            foreach (Member item in MemberList)
+            {
+                item.AgePartitioning();
+            }
         }
     }
 }
