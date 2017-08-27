22412df4462946178fc62f163eee800889bcb585
Ditte <Ditte@DESKTOP-QORFADN>
2017-08-27 22:12:18 +0200

    "Member" class added and "done"

---
+++ RecapOpgave/Member.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapOpgave
+{
+    public class Member
+    {
+
+        //PROPERTIES
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Team { get; set; }
+
+        //CONSTRUCTOR
+        public Member(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+        }
+
+        //METHODS
+        public void AgePartitioning()
+        {
+            if (Age < 18)
+            {
+                Team = "Pandaholdet";
+            }
+            else
+            {
+                Team = "Drageoldet";
+            }
+
+            Console.WriteLine(Name + " skal begynde på " + Team);
+        }
+
+
+
+    }
+}
